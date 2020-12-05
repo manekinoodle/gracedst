2d1f24b4c43bd94912eac116c520d9ba8dae0e32
Pyro <onetruejake@outlook.com>
2020-12-05 21:50:12 +1100

    Made Grace the only one that can craft her unique items

--- scripts/prefabs/grace.lua
+++ scripts/prefabs/grace.lua
@@ -74,6 +74,8 @@ local master_postinit = function(inst)
 
 	--This is probably what allows wendy to interact with pipspooks but idk if I'm using it right
 	inst:AddTag("ghostlyfriend")
+	--builder tag
+	inst:AddTag("gravekeeper")
 
 	--WX78 had this tag, not sure if it's what gives him his spoilage immunity
 	if inst.components.eater ~= nil then
