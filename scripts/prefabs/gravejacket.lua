be4e369058dd4f2e3b91748406a5106413957169
Moki T <mokichan1001@yahoo.com>
2020-12-26 17:28:51 +1000

    fixed sleeves, still need ground sprite

--- scripts/prefabs/gravejacket.lua
+++ scripts/prefabs/gravejacket.lua
@@ -1,28 +1,29 @@
 local assets =
 {
-	Asset("ANIM", "anim/gravejacket_ground.zip"), 
+	Asset("ANIM", "anim/gravejacket_ground.zip"),
 	Asset("ANIM", "anim/swap_gravejacket.zip"),
+	Asset("ANIM", "anim/gravejacket_skin.zip"),
 
-    Asset("ATLAS", "images/inventoryimages/gravejacket.xml"), 
-    Asset("IMAGE", "images/inventoryimages/gravejacket.tex"), 
+    Asset("ATLAS", "images/inventoryimages/gravejacket.xml"),
+    Asset("IMAGE", "images/inventoryimages/gravejacket.tex"),
 
 }
 
-local function onequippedskinitem_xmasuit(inst) -- in case the player uses wardrobe while xmasuit is equipped
-    if inst.components.inventory 
-	and inst.components.inventory:GetEquippedItem(EQUIPSLOTS.BODY) ~= nil 
-	and inst.components.inventory:GetEquippedItem(EQUIPSLOTS.BODY):HasTag("xmasuit")
+local function onequippedskinitem_gravejacket(inst) -- in case the player uses wardrobe while gravejacket is equipped
+    if inst.components.inventory
+	and inst.components.inventory:GetEquippedItem(EQUIPSLOTS.BODY) ~= nil
+	and inst.components.inventory:GetEquippedItem(EQUIPSLOTS.BODY):HasTag("gravejacket")
 	and inst.components.skinner then
-	
-		local xmasuit = inst.components.inventory:GetEquippedItem(EQUIPSLOTS.BODY)
-		
-		if inst.components.skinner.clothing["body"] ~= nil 
+
+		local gravejacket = inst.components.inventory:GetEquippedItem(EQUIPSLOTS.BODY)
+
+		if inst.components.skinner.clothing["body"] ~= nil
 		and inst.components.skinner.clothing["body"] ~= "" then
-			xmasuit.bodyskin = inst.components.skinner.clothing["body"]
+			gravejacket.bodyskin = inst.components.skinner.clothing["body"]
 		end
-		
-		if xmasuit then
-			inst:DoTaskInTime(0.5, function() xmasuit.reequip(xmasuit, inst) end)
+
+		if gravejacket then
+			inst:DoTaskInTime(0.5, function() gravejacket.reequip(gravejacket, inst) end)
 		end
 	end
 end
@@ -37,20 +38,20 @@ local function onequip(inst, owner)
 		owner.components.skinner.clothing["body"] = ""
 		owner.components.skinner:ClearClothing("body")
 	end
-	
+
 	local tuck_torso = BASE_TORSO_TUCK[owner.prefab] and BASE_TORSO_TUCK[owner.prefab] == "full"
 	if tuck_torso then --if torso is tucked then we have to switch symbols around
 		owner.components.skinner:SetSkinMode()
-		owner.AnimState:SetSymbolExchange( "torso_pelvis", "torso" ) -- switch body parts around 
+		owner.AnimState:SetSymbolExchange( "torso_pelvis", "torso" ) -- switch body parts around
 		owner.AnimState:OverrideSkinSymbol("torso_pelvis", "gravejacket", "torso" )
 	end
 
 	owner.AnimState:OverrideSymbol("arm_lower", "gravejacket_skin", "arm_lower")
 	owner.AnimState:OverrideSymbol("arm_upper", "gravejacket_skin", "arm_upper")
 
-	owner:ListenForEvent("equipskinneditem", onequippedskinitem_xmasuit)
-	owner:ListenForEvent("unequipskinneditem", onequippedskinitem_xmasuit)
-	
+	owner:ListenForEvent("equipskinneditem", onequippedskinitem_gravejacket)
+	owner:ListenForEvent("unequipskinneditem", onequippedskinitem_gravejacket)
+
 end
 
 local function onunequip(inst, owner)
@@ -60,11 +61,11 @@ local function onunequip(inst, owner)
 	owner.AnimState:ClearOverrideSymbol("arm_lower")
 	owner.AnimState:ClearOverrideSymbol("arm_upper")
 	owner.AnimState:ClearOverrideSymbol("torso")
-	
+
 	if owner.CurrentModdedSkin ~= nil then
-		owner:RemoveEventCallback("onchangemoddedskin", onequippedskinitem_xmasuit)
+		owner:RemoveEventCallback("onchangemoddedskin", onequippedskinitem_gravejacket)
 	end
-	
+
 	owner.AnimState:ClearOverrideSymbol("swap_body")
 
 	if inst.bodyskin ~= nil then
@@ -72,10 +73,25 @@ local function onunequip(inst, owner)
 		owner.components.skinner:SetSkinMode()
 		inst.bodyskin = nil
 	end
-	
+
 	local tuck_torso = BASE_TORSO_TUCK[owner.prefab] and BASE_TORSO_TUCK[owner.prefab] == "full"
-	if tuck_torso then 
+	if tuck_torso then
 		owner.components.skinner:SetSkinMode() -- fix switched body parts
+	end
+
+	owner:RemoveEventCallback("equipskinneditem", onequippedskinitem_gravejacket)
+	owner:RemoveEventCallback("unequipskinneditem", onequippedskinitem_gravejacket)
+end
+
+local function onpreload(inst, data)
+	if data ~= nil and data.bodyskin ~= nil and inst.bodyskin == nil then
+		inst.bodyskin = data.bodyskin
+	end
+end
+
+local function onsave(inst, data)
+	if data and inst.bodyskin ~= nil then
+		data.bodyskin = inst.bodyskin
 	end
 end
 
@@ -94,9 +110,9 @@ local function fn()
         return inst
     end
 
-    inst.AnimState:SetBank("swap_gravejacket")
-    inst.AnimState:SetBuild("swap_gravejacket")
-    inst.AnimState:PlayAnimation("anim")
+    inst.AnimState:SetBank("gravejacket_ground")
+    inst.AnimState:SetBuild("gravejacket_ground")
+    inst.AnimState:PlayAnimation("gravejacket_ground")
 
     inst:AddComponent("inspectable")
 
@@ -106,7 +122,6 @@ local function fn()
 
     inst:AddComponent("equippable")
     inst.components.equippable.equipslot = EQUIPSLOTS.BODY
-    inst.components.equippable.dapperness = TUNING.DAPPERNESS_SMALL
 
     inst.components.equippable:SetOnEquip( onequip )
     inst.components.equippable:SetOnUnequip( onunequip )
@@ -117,9 +132,23 @@ local function fn()
     inst.components.fueled:SetDepletedFn(inst.Remove)
 
 	inst:AddComponent("insulator")
-    inst.components.insulator:SetInsulation( TUNING.INSULATION_LARGE )
+    inst.components.insulator:SetInsulation( TUNING.INSULATION_MED )
 
-	inst:AddTag("xmasuit")
+	inst:AddComponent("waterproofer")
+
+	inst:AddTag("gravejacket")
+
+	inst.reequip = onequip
+
+	inst.OnSave = onsave
+	inst.OnPreLoad = onpreload
+
+	inst:DoTaskInTime(0, function() -- fixes game not overriding symbols on game load
+	if inst.components.inventoryitem ~= nil and inst.components.inventoryitem.owner ~= nil and inst.components.equippable:IsEquipped() then
+		local owner = inst.components.inventoryitem.owner
+		onequip(inst, owner)
+	end
+	end)
 
     MakeHauntableLaunch(inst)
 
