b1806f4b25d86938ff9f04a119a35c7545fda241
Moki T <mokichan1001@yahoo.com>
2020-12-30 13:46:00 +1000

    grace has a neck now

--- modinfo.lua
+++ modinfo.lua
@@ -1,11 +1,11 @@
 -- This information tells other players more about the mod
 name = "Grace"
-description = "Play as Grace from Pathologic. Version 0.6"
+description = "Play as Grace from Pathologic. \n\nVersion 0.61"
 author = "ManekiNoodle and PyrotechnicCake"
-version = "0.6" -- This is the version of the template. Change it to your own number.
+version = "0.61" -- This is the version of the template. Change it to your own number.
 
 -- This is the URL name of the mod's thread on the forum; the part after the ? and before the first & in the url
-forumthread = "/files/file/950-extended-sample-character/"
+forumthread = "nil"
 
 -- This lets other players know if your mod is out of date, update it to match the current version in the game
 api_version = 10
